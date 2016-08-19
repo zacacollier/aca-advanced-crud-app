7a9906b53212adfa6fc8c2bc1a0ce7ffa92cbc7f
Canaan Davis <canaandavis@gmail.com>
2016-08-18 19:41:20 -0500

    Rename rotues to routes

--- rotues/posts.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// TODO: Add your routes to the route here.
-// Hint: Don't for get to export it!
--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,3 @@ const router = express.Router();
 
 // TODO: Add your routes to the route here.
 // Hint: Don't for get to export it!
-
-module.exports = router;
